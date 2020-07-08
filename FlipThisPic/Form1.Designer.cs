3ee35ceef84e798b35900dc5b696743a528313ac
Nadav Weisler <32937058+nadavWeisler@users.noreply.github.com>
2020-07-08 12:02:12 +0300

    Init

---
+++ FlipThisPic/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace FlipThisPic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Covert = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SelectFilesButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.CovertButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.directoryEntry1 = new System.DirectoryServices.DirectoryEntry();
+            this.Covert.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Covert
+            // 
+            this.Covert.Controls.Add(this.numericUpDown1);
+            this.Covert.Controls.Add(this.label1);
+            this.Covert.Controls.Add(this.textBox2);
+            this.Covert.Controls.Add(this.label3);
+            this.Covert.Controls.Add(this.SelectFilesButton);
+            this.Covert.Controls.Add(this.textBox1);
+            this.Covert.Controls.Add(this.CovertButton);
+            this.Covert.Location = new System.Drawing.Point(13, 13);
+            this.Covert.Name = "Covert";
+            this.Covert.Size = new System.Drawing.Size(409, 161);
+            this.Covert.TabIndex = 1;
+            this.Covert.TabStop = false;
+            this.Covert.Text = "Flip This";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(143, 107);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(209, 26);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Select Folder";
+            // 
+            // SelectFilesButton
+            // 
+            this.SelectFilesButton.Location = new System.Drawing.Point(358, 32);
+            this.SelectFilesButton.Name = "SelectFilesButton";
+            this.SelectFilesButton.Size = new System.Drawing.Size(45, 26);
+            this.SelectFilesButton.TabIndex = 6;
+            this.SelectFilesButton.Text = "...";
+            this.SelectFilesButton.UseVisualStyleBackColor = true;
+            this.SelectFilesButton.Click += new System.EventHandler(this.SelectFilesButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(143, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(209, 26);
+            this.textBox1.TabIndex = 5;
+            // 
+            // CovertButton
+            // 
+            this.CovertButton.Location = new System.Drawing.Point(6, 96);
+            this.CovertButton.Name = "CovertButton";
+            this.CovertButton.Size = new System.Drawing.Size(115, 48);
+            this.CovertButton.TabIndex = 0;
+            this.CovertButton.Text = "Flip";
+            this.CovertButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 20);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Degree";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(143, 64);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            359,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(209, 26);
+            this.numericUpDown1.TabIndex = 13;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            90,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 182);
+            this.Controls.Add(this.Covert);
+            this.Name = "Form1";
+            this.Text = "Flip this pic";
+            this.Covert.ResumeLayout(false);
+            this.Covert.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox Covert;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button SelectFilesButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button CovertButton;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.DirectoryServices.DirectoryEntry directoryEntry1;
+    }
+}
+
